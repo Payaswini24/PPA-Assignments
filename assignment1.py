2c409449cb3cd56c197ca7c915dbdccdce3fbfc9
Payaswini <47058155+Payaswini24@users.noreply.github.com>
2020-04-17 21:43:07 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+print("code for Assignment 1 :")
+
+disp= dict()
+
+for i in range(0,3):
+	name=input("Enter Name :")
+	usn= input("Enter Usn :")
+	disp[usn]=name 
+
+print(disp)
+
+#for key,value in disp.items():
+	#print(key,':',value) 
