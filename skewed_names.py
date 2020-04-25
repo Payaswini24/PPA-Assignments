3ee4a707f7a20d6068d3445b342bb0e509510675
Payaswini <47058155+itspayaswini@users.noreply.github.com>
2020-04-25 18:24:52 +0530

    Add files via upload

---
+++ skewed_names.py
@@ -0,0 +1,17 @@
+import re
+
+while True:
+	inp=input("enter the name.Type stop when done\n")
+	if (inp)=="stop":
+		break
+	else: 
+		name=[]
+		string=" "
+		for char in inp:
+			if char.isalpha():
+				name.append(char)
+		temp=''.join(name)
+		final=re.findall('[A-Z][a-z]*',temp)
+		name=string.join(final)
+		print(name)
+
