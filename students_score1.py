b0e0859a3fb06745ec544c844d5d948ea585f10f
Payaswini <47058155+itspayaswini@users.noreply.github.com>
2020-04-28 18:50:44 +0530

    Update students_score1.py

--- students_score1.py
+++ students_score1.py
@@ -1,46 +1,17 @@
-def score(name,m1,p1,c1,b1,ss1):
-	mydict={"Name":name,"Math":m1,"Physics":p1,"Chemistry":c1,"Biology":b1,"Social-Science":ss1}
-	while True:
-			inp2=input("enter the name and subject.Type stop to end ")
-			if inp2==("stop"):
-				break
-			else:
-				nam,sub=inp2.split()
-				for k,v in mydict.items():
-					if k==sub:
-						print(name,"scored",v,"in",sub)
+mydict={}
 
-def myfu(ele):
-	x,a,b,c,d,e=ele.split()
-	score(x,a,b,c,d,e)
+subject=["Math","Physics","Chemistry","Biolog","Social-Science"]
 
-i=6
-lst=[]
-while(i>2):
-	inp1=input("enter NAME AND MARKS\n")
-	if inp1=="stop":
+while True:
+	inp=input("enter the student's name and marks ")
+
+	if inp=="":
 		break
-	else:
-		i=i-1
-		lst.append(inp1)
-for ele in lst:
-	sp=str(lst).strip('[]')
-	print(sp)
-	x=map(myfu(ele),ele)
-#sub=sp.split(' ')
-#print(sub)
-
-#res.append(sub)
-#print(res)
-	
-	#final=dict(zip(lst2,ele))
-	#print((final))
-
-#for ele in lst:
-#	sp=str(lst).strip('[]')
-	
-
-#	
+	inp=inp.split()
+	a,b=inp[0],inp[1:]
+	mydict[a]=list(map(int,b))
 
 
-
+name,sub=input("enter Name & Subject ").split()
+i=subject.index(sub)
+print(f"{name} scored {mydict[name][i]} in {sub}" )
