7ac4c7e12107553484aafc34230ff2340280ddac
Payaswini <47058155+itspayaswini@users.noreply.github.com>
2020-04-28 20:39:09 +0530

    Update student_score2.py

--- student_score2.py
+++ student_score2.py
@@ -1,11 +1,12 @@
-print("Alex 78 78 67 56 89\nSteve 54 67 89 87 65\nTrish 56 78 87 89 90\nMartin 48 89 85 82 81")
-inp1=input("choose a subject\n")
+mydict={'Alex': [50, 98, 69, 78, 89], 'Steve': [78, 87, 98, 88, 79], 
+		'Martin': [45, 99, 100, 89, 88], 'Trish': [66, 68, 70, 71, 68]}
+lst=[]
 
-mydict={"Name":["Alex","Steve,Trish,Martin"],"Math":[78,54,56,48],"Physics":[78,67,78,89],"Chemistry":[67,89,87,85],
-		"Biology":[56,87,89,82],"Social-Science":[89,65,90,81]}
-for k,v in mydict.items():
-	if k==inp1:
-		j=(str(mydict["Name"])).strip('[]')
-		print(j)
-		mrks=(str(v)).strip('[]')
-		print(mrks)
+subject=["Math","Physics","Chemistry","Biology","Social-Science"]
+
+sub=input("enter the subject ")
+i=subject.index(sub)
+lst=[(k,v[i])for k,v in mydict.items()]
+#print(lst)
+tup=sorted(lst, key = lambda x: x[1],reverse=True) 
+print(tup)
