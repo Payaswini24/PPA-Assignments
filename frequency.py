3b570c1ebccbbfda22b6ac7103a39434a06760f2
Payaswini <47058155+itspayaswini@users.noreply.github.com>
2020-05-08 21:40:46 +0530

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,13 @@
+lst=[]
+
+inp=input("enter the string\n")
+#inp=inp.capitalize()
+inp=list(inp.split(" "))
+inp.sort()
+
+for i in inp:
+	if i not in lst:
+		lst.append(i)
+
+for i in range(0,len(lst)):
+	print(f"{lst[i]}: {inp.count(lst[i])}")
